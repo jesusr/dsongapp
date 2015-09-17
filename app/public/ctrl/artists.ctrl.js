4474f95fec522c22d293c55df1abdcaa8ca42263
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-09-17 12:10:15 +0200

    Artist single and list ctrl

--- app/public/ctrl/artists.ctrl.js
+++ app/public/ctrl/artists.ctrl.js
@@ -1,20 +1,22 @@
-(function(){
-  var dsong= angular.module('dsong');
-  dsong.controller('artistsListCtrl', ['$scope','$location','$routeParams', '$http',
-    function($scope,$location,$routeParams,$http) {
-      if ($routeParams.page) page=$routeParams.page;
-      else page=1;
-      $http.get('/api/artists/15/'+page)
-        .success(function(data) {
-          $scope.artists = data.data;
-          $scope.page = data.page;
-          $scope.ptotal = data.pageTotal;
-          $scope.pnext = (data.pageTotal === page ? -1 : parseInt(page)+1);
-          $scope.pprev = (page<2 ? -1 : page-1);
-          $scope.dataExist = function(attr){
-            if(attr == null || attr == "") return false;
-            return attr.length;
-          };
-        });
-    }]);
-}());
+
+  (function(){
+    var dsong= angular.module('dsong');
+    console.log(dsong);
+    dsong.controller('artistsListCtrl', ['$scope','$location','$routeParams', '$http',
+      function($scope,$location,$routeParams,$http) {
+        if ($routeParams.page) page=$routeParams.page;
+        else page=1;
+        $http.get('/api/artists/15/'+page)
+          .success(function(data) {
+            $scope.artists = data.data;
+            $scope.page = data.page;
+            $scope.ptotal = data.pageTotal;
+            $scope.pnext = (data.pageTotal === page ? -1 : parseInt(page)+1);
+            $scope.pprev = (page<2 ? -1 : page-1);
+            $scope.dataExist = function(attr){
+              if(attr == null || attr == "") return false;
+              return attr.length;
+            };
+          });
+      }]);
+  }());
