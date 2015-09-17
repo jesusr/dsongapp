d5cfb3632cd769961180e0a14f1bd3d5c445dc29
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-09-17 12:30:47 +0200

    artist.ctrl.js validation

--- app/public/ctrl/artist.ctrl.js
+++ app/public/ctrl/artist.ctrl.js
@@ -1,18 +1,21 @@
+var dsong = angular.module('dsong');
+dsong.controller('artistDetailCtrl', ['$scope', '$http', '$routeParams',
+    function($scope, $http, $routeParams) {
+        var id = $routeParams.artistId;
+        $http.get('/api/artist/' + id + '/songs')
+            .success(function(data) {
+                $scope.songs = data;
+                $scope.dataExist = function(attr) {
+                    if (attr === null || attr === '') {
+                        return false;
+                    }
+                    return attr.length;
+                };
+            });
+        $http.get('/api/artist/' + id)
+            .success(function(data) {
+                data.length ? $scope.artist = data[0] : $scope.artist = false;
+            });
+    }
+]);
 
-  var dsong= angular.module('dsong');
-  dsong.controller('artistDetailCtrl', ['$scope', '$http', '$routeParams',
-    function ($scope, $http, $routeParams) {
-      var id = $routeParams.artistId;
-      $http.get('/api/artist/'+id+'/songs')
-        .success(function(data) {
-          $scope.songs = data;
-          $scope.dataExist = function(attr){
-            if(attr == null || attr == "") return false;
-            return attr.length;
-          };
-        });
-      $http.get('/api/artist/'+id)
-        .success(function(data) {
-          data.length ? $scope.artist = data[0] : $scope.artist = false; 
-        });
-    }]);
