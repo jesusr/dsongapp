483bf27508e78accb59b152245f406651bbf2bde
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-09-17 12:34:24 +0200

    home.ctrl.js validation

--- app/public/ctrl/home.ctrl.js
+++ app/public/ctrl/home.ctrl.js
@@ -1,25 +1,38 @@
+(function() {
+    var dsong = angular.module('dsong');
+    dsong.controller('homeCtrl', ['$scope', '$location', '$http', 'flickrPhotos',
+        function($scope, $location, $http, flickrPhotos) {
+            $http.get('/api/artists/random/4')
+                .success(function(data) {
+                    var e;
+                    $scope.artists = data;
+                    $scope.img = [];
 
-  (function(){
-    var dsong= angular.module('dsong');
-    dsong.controller('homeCtrl', ['$scope','$location', '$http', 'flickrPhotos',
-      function($scope, $location, $http, flickrPhotos){
-        $http.get('/api/artists/random/4')
-          .success(function(data){
-            $scope.artists = data;
-            $scope.img = [];
-            for (e in data){
-              flickrPhotos.load({ tags: data[e].name +' , live', tag_mode : 'all' , content_type: 1, per_page: 1},function(data){
-                if(!data.items.length) $scope.img.push('/assets/res/img/dummy.png');
-                else $scope.img.push(data.items[0].media.m);
-              });   
-            }
-          });
-        $http.get('/api/songs/random/10')
-          .success(function(data){
-            $scope.songs = data;
-            $scope.img_songs = [];
-            for (e in data){
-            }
-          });  
-      }]);
-  })();
+                    function callback(data) {
+                        if (!data.items.length) {
+                            $scope.img.push('/assets/res/img/dummy.png');
+                        } else {
+                            $scope.img.push(data.items[0].media.m);
+                        }
+                    }
+
+                    for (e in data) {
+                        flickrPhotos.load({
+                            tags: data[e].name + ' , live',
+                            tag_mode: 'all',
+                            content_type: 1,
+                            per_page: 1
+                        }, callback(data));
+                    }
+                });
+            $http.get('/api/songs/random/10')
+                .success(function(data) {
+                    var e;
+                    $scope.songs = data;
+                    $scope.img_songs = [];
+                    for (e in data) {}
+                });
+        }
+    ]);
+})();
+
