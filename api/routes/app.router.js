8ae7a5c56ac4f66f9ed015450cca87af4ac08d91
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-09-17 12:09:06 +0200

    Router api clean

--- api/routes/app.router.js
+++ api/routes/app.router.js
@@ -2,8 +2,5 @@ var express = require('express');
 var app = app || express();
 var approuter = express.Router();
 
-// approuter.use(function(req, res) {
-//   res.sendfile('./public/index.html')
-// });
 
 module.exports = approuter;
