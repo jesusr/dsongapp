23104fcbc79ea14c1374c81217232d8a1d1358a8
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-09-17 12:11:04 +0200

    paginator

--- app/public/ctrl/pag.ctrl.js
+++ app/public/ctrl/pag.ctrl.js
@@ -1,7 +1,8 @@
-(function(){
-  var dsong= angular.module('dsong');
-  dsong.controller('pagCtrl', ['$scope','$location','$routeParams', 
-    function($scope,$location,$routeParams) {
-      
-  }]);
-}());
+
+  (function(){
+    var dsong= angular.module('dsong');
+    dsong.controller('pagCtrl', ['$scope','$location','$routeParams', 
+      function($scope,$location,$routeParams) {
+        
+    }]);
+  }());
