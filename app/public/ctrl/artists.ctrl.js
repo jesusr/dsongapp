11816472ef5e55f9db874d6da0c17c0a4ce71aae
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-09-17 12:32:20 +0200

    artists.ctrl.js validation

--- app/public/ctrl/artists.ctrl.js
+++ app/public/ctrl/artists.ctrl.js
@@ -1,22 +1,29 @@
-
-  (function(){
-    var dsong= angular.module('dsong');
+(function() {
+    var dsong = angular.module('dsong');
     console.log(dsong);
-    dsong.controller('artistsListCtrl', ['$scope','$location','$routeParams', '$http',
-      function($scope,$location,$routeParams,$http) {
-        if ($routeParams.page) page=$routeParams.page;
-        else page=1;
-        $http.get('/api/artists/15/'+page)
-          .success(function(data) {
-            $scope.artists = data.data;
-            $scope.page = data.page;
-            $scope.ptotal = data.pageTotal;
-            $scope.pnext = (data.pageTotal === page ? -1 : parseInt(page)+1);
-            $scope.pprev = (page<2 ? -1 : page-1);
-            $scope.dataExist = function(attr){
-              if(attr == null || attr == "") return false;
-              return attr.length;
-            };
-          });
-      }]);
-  }());
+    dsong.controller('artistsListCtrl', ['$scope', '$location', '$routeParams', '$http',
+        function($scope, $location, $routeParams, $http) {
+            var page;
+            if ($routeParams.page) {
+                page = $routeParams.page;
+            } else {
+                page = 1;
+            }
+            $http.get('/api/artists/15/' + page)
+                .success(function(data) {
+                    $scope.artists = data.data;
+                    $scope.page = data.page;
+                    $scope.ptotal = data.pageTotal;
+                    $scope.pnext = (data.pageTotal === page ? -1 : parseInt(page) + 1);
+                    $scope.pprev = (page < 2 ? -1 : page - 1);
+                    $scope.dataExist = function(attr) {
+                        if (attr === null || attr === '') {
+                            return false;
+                        }
+                        return attr.length;
+                    };
+                });
+        }
+    ]);
+}());
+
